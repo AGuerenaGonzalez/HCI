fc1d0181190e2a0eeb8b95f2c2ce320dcb98e888
AGuerenaGonzalez <97479906+AGuerenaGonzalez@users.noreply.github.com>
2023-03-13 17:11:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,67 @@
 document.getElementById("add-project").addEventListener("click", addRow);
+document.getElementById("arrow-button").addEventListener("click", returnScreen);
 
 function addRow() {
     let projectName = prompt("Enter a project name:");
     if (projectName != null) {
         let newRow = document.createElement("tr");
         let newCell = document.createElement("td");
-        let newButton = document.createElement("button")
-        newButton.id = projectName;
-        newButton.textContent = projectName;
-        newButton.addEventListener("click", navigateProject);
-        newCell.appendChild(newButton);
+        let newProjectButton = document.createElement("button")
+        let newRenameButton = document.createElement("button");
+        let newDeleteButton = document.createElement("button");
+
+        newProjectButton.id = projectName;
+        newProjectButton.className = "project-button"
+        newProjectButton.textContent = projectName;
+        newProjectButton.addEventListener("click", navigateToRoomScreen);
+
+        newRenameButton.id = "rename" + projectName;
+        newRenameButton.className = "rename-button";
+        newRenameButton.addEventListener("click", rename);
+
+        newDeleteButton.id = "delete" + projectName;
+        newDeleteButton.className = "delete-button";
+        newDeleteButton.addEventListener("click", del);
+
+        newCell.appendChild(newProjectButton);
+        newCell.appendChild(newRenameButton);
+        newCell.appendChild(newDeleteButton);
         newRow.appendChild(newCell);
         document.getElementById("project-table").appendChild(newRow);
     }
 }
+function returnScreen(){
+    document.getElementById("room-screen").classList.add("hidden");
+    document.getElementById("project-menu-screen").classList.remove("hidden");
 
-function navigateProject(event){
-    let name = event.target.id;
+    document.getElementById("project-title").remove();
 }
+
+function rename(event){
+    const projectButton = event.target.parentNode.querySelector(".project-button");
+    let newProjectName = prompt("Enter a new project name:");
+
+    if(newProjectName != null){
+        projectButton.innerText = newProjectName;
+        projectButton.id = newProjectName;
+    }
+}
+
+function del(event){
+    const row = event.target.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+
+function navigateToRoomScreen(event){
+    document.getElementById("project-menu-screen").classList.add("hidden");
+    document.getElementById("room-screen").classList.remove("hidden");
+
+    const title = document.createElement("h1");
+    title.id = "project-title";
+    const text = document.createTextNode(event.target.id);
+    title.appendChild(text);
+
+    document.getElementById("room-screen").appendChild(title);
+}
+
+
